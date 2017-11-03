642f14c6f4842e2aa515dc22633826d6ae699eeb
Luckas Fiege <lsfiege@gmail.com>
2017-11-03 14:52:30 -0300

    Fix comentarios en ORM

--- ORM/redbeam.php
+++ ORM/redbeam.php
@@ -7,15 +7,14 @@ require ('env.php');
 R::setup('pgsql:host='.$host.';dbname='.$db_name,
             $db_user_name,$db_password);
     
-/*R::setup('pgsql:host=localhost;dbname=shawnprueba',
-        'postgres','postgres');*/
-
-
 R::debug( false );
 
 
 /**
  * Conectar a la base de datos por linea de comandos
  * sudo -u postgres psql
+ * Desde docker, conectarse al container www y ejecutar
+ * psql -h db -p 5432 -U postgres
+ * Donde db es el nombre del container de la base de datos
  */
 ?>
