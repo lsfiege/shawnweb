1d4cd1f1373e0f4bdad0751c2fe9ca491abe42a7
Luckas Fiege <lsfiege@gmail.com>
2018-01-24 11:57:03 -0300

    Fix pagina modificar datos usuario + capitalize username in header

--- modulos/usuarios/vistas/modificar_datos_perfil.php
+++ modulos/usuarios/vistas/modificar_datos_perfil.php
@@ -1,64 +1,88 @@
 <?php
 //inicia la sesion
 session_start();
-if (($_SESSION['Usuario'] == null))
-{
+if (($_SESSION['Usuario'] == null)) {
     header('Location: http://'.$_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_PORT'].'/modulos/login/controlador/logout.class.php');
 }
-require ($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'modelo'.DIRECTORY_SEPARATOR.'Usuario.class.php');
-$usuario=  unserialize($_SESSION['Usuario']);
+require($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'modelo'.DIRECTORY_SEPARATOR.'Usuario.class.php');
+$usuario = unserialize($_SESSION['Usuario']);
 ?>
 
-<?php 
-require ($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'header.php');
-?>
-</head>
+<?php
+require($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'header.php'); ?>
 
-<body>
-<?php require ($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'header_internas.php'); ?>
-    <h2 class="titulo_home_login home_login_subtitulo2">Modificaci&oacute;n de Datos de Perfil</h2>
-    <h3 class="titulo_home_login home_login_subtitulo3">Complete el formulario para modifcar los datos del perfil</h3>    
-    <?php require_once ($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'mensajes_notificacion.php'); ?>
-    <form action="/modulos/usuarios/controlador/usuarios.class.php" method="POST">
-        <div class="lbls_login">
-            <div class="lbl_parametro">
-                <label>Nombre de Usuario*</label>
-            </div>  
-            <div class="lbl_parametro">
-                <label>E-mail*</label>
-            </div>
-            <div class="lbl_parametro">
-                 <label>Contrase&ntilde;a*</label>
-            </div>
-            <div class="lbl_parametro">
-                 <label>Repetir Contrase&ntilde;a*</label>
-            </div>
-            <div class="lbl_parametro">                
-            </div>
-            <div class="lbl_parametro">                
-            </div>
-        </div>     
-        <div class="inps_login">
-            <div class="txt_parametro" >
-                <input type="text" name="nombre-usuario" value="<?php echo $usuario->getNombre(); ?>" size="16" autofocus required>
-            </div>
-            <div class="txt_parametro" >
-                 <input type="email" name="email" value="<?php echo $usuario->getEmail(); ?>" size="22" required>
-            </div>
-            <div class="txt_parametro" >
-                <input type="password" name="password" value="" required>
-            </div>
-            <div class="txt_parametro" >
-                <input type="password" name="password-confirmar" value="" required>
-            </div>
-            <div class="txt_parametro" >
-                <label>(*) Campos obligatorios</label>
-            </div>    
-            <div class="txt_parametro" >
-                <input type="submit" value="Modificar datos de perfil" name="modificar-usuario" />
-            </div>
-        </div>                
+<div class="col">
+    <a class="btn btn-sm btn-outline-secondary float-left"
+       href=" <?= 'http://'.$_SERVER['SERVER_NAME'].':'.$_SERVER['SERVER_PORT'].'/modulos/simulacion/vistas/simulacion_wsn.php' ?> ">
+        Volver
+    </a>
+</div>
+
+<div class="container-fluid">
+
+    <div class="center-block text-center">
+        <h2 class="form-signin-heading">Modificación de Datos de Perfil</h2>
+
+        <?php require_once($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'mensajes_notificacion.php'); ?>
+    </div>
+
+    <form action="/modulos/usuarios/controlador/usuarios.class.php" method="POST" class="form-signin">
+
+        <div class="form-group">
+            <label for="nombre-usuario">Nombre de Usuario*</label>
+            <input type="text"
+                   id="nombre-usuario"
+                   name="nombre-usuario"
+                   class="form-control"
+                   value="<?php echo $usuario->getNombre(); ?>"
+                   autofocus
+                   required>
+        </div>
+
+        <div class="form-group">
+            <label for="email">E-mail*</label>
+            <input type="email"
+                   id="email"
+                   name="email"
+                   class="form-control"
+                   value="<?php echo $usuario->getEmail(); ?>"
+                   required>
+        </div>
+
+        <div class="form-group">
+            <label for="password">Contraseña*</label>
+            <input type="password"
+                   id="password"
+                   name="password"
+                   class="form-control"
+                   value=""
+                   required>
+        </div>
+
+        <div class="form-group">
+            <label for="password-confirmar">Repetir Contraseña*</label>
+            <input type="password"
+                   id="password-confirmar"
+                   name="password-confirmar"
+                   class="form-control"
+                   value=""
+                   required>
+        </div>
+
+        <small id="form-help" class="form-text text-muted">(*)Campos obligatorios</small>
+
+        <div class="form-group">
+            <input type="submit" class="btn btn-block btn-primary" value="Modificar datos de perfil"
+                   name="modificar-usuario"/>
+        </div>
     </form>
-<?php require_once ($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'footer.php'); ?>    
+
+    <div class="center-block text-center">
+        <p>Complete el formulario para modifcar los datos del perfil</p>
+    </div>
+
+</div>
+
+<?php require_once($_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'lib'.DIRECTORY_SEPARATOR.'footer.php'); ?>
 </body>
 </html>
